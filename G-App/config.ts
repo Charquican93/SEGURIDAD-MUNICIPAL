db34fb842bf982709a313e25890edf0150c02dba
Leonardo A Gonzalez C <leonardo-gonzalez@live.cl>
2026-02-11 09:27:51 -0300

    Test 01 Railway API URL

--- G-App/config.ts
+++ G-App/config.ts
@@ -1,3 +1,3 @@
 //export const API_URL = 'http://192.168.199.219:3001';
-export const API_URL = 'http://192.168.1.83:3001'; // ip de mi house 
-//export const API_URL = 'https://ccg-app-production.up.railway.app'; // Pega aquí la URL del Paso 2 (sin barra al final)
+//export const API_URL = 'http://192.168.1.83:3001'; // ip de mi house 
+export const API_URL = 'seguridad-municipal-production.up.railway.app'; // Pega aquí la URL del Paso 2 (sin barra al final)
