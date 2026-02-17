cd4888da37d452f01f8969f4384b4c68d27e3ab0
Leonardo A Gonzalez C <leonardo-gonzalez@live.cl>
2026-02-17 16:48:00 -0300

    Conexion remota

--- G-App/config.ts
+++ G-App/config.ts
@@ -1,3 +1,3 @@
 //export const API_URL = 'http://localhost:3001'; OFICINA
-export const API_URL = 'http://192.168.1.88:3001'; // ip de mi house 
-//export const API_URL = 'https://seguridad-municipal-production.up.railway.app'; // RAILWAY
+//export const API_URL = 'http://192.168.1.88:3001'; // ip de mi house 
+export const API_URL = 'https://seguridad-municipal-production.up.railway.app'; // RAILWAY
